2eb3ccb99ee6096ee27006f6c0d6759191afe479
emjja <139229750+emjja@users.noreply.github.com>
2023-07-13 22:34:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "globals" {
   source = "git::https://git.gft.com/gcp_landing_zone/modules/globals.git?ref=mjja"
 }
 
-/
+/*
 // Because there is a need to add permissions on org level and SA do not have any permisions there, we need to add these roles through Cloud Console.
 resource "google_organization_iam_binding" "scc-service-account-iam-1" {
   org_id  = var.organization_id
