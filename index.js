7b95af73aad5e68d3f5504eb25d082352fe29d22
emjja <139229750+emjja@users.noreply.github.com>
2023-07-14 14:51:11 +0200

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-def vulnerable_function(user_input):
-  # This code is vulnerable to a SQL injection attack.
-  # The user input is not properly escaped, so it could be used to inject malicious SQL into the database.
-  return "SELECT * FROM users WHERE username = '" + user_input + "'"
-
-if __name__ == "__main__":
-  user_input = "'; DROP TABLE users; --"
-  print(vulnerable_function(user_input))
