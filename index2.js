743d03986b124157de79914c2271ee8c752058f5
emjja <139229750+emjja@users.noreply.github.com>
2023-07-14 14:51:18 +0200

    Delete index2.js

--- index2.js
+++
@@ -1,8 +0,0 @@
-def vulnerable_function(user_input):
-  # This code is vulnerable to a command injection attack.
-  # The user input is not properly escaped, so it could be used to execute arbitrary commands on the system.
-  return os.system(user_input)
-
-if __name__ == "__main__":
-  user_input = "echo 'This is a command injection'"
-  print(vulnerable_function(user_input))
