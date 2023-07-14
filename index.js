d685bf0f19c3330eb23b3dce35f7677ac6cf43d1
emjja <139229750+emjja@users.noreply.github.com>
2023-07-14 14:31:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 def vulnerable_function(user_input):
-  # This code is vulnerable to a cross-site scripting (XSS) attack.
-  # The user input is not properly escaped, so it could be used to inject malicious code into the page.
-  return "<script>alert(document.cookie)</script>" + user_input
+  # This code is vulnerable to a SQL injection attack.
+  # The user input is not properly escaped, so it could be used to inject malicious SQL into the database.
+  return "SELECT * FROM users WHERE username = '" + user_input + "'"
 
 if __name__ == "__main__":
-  user_input = "Hello, world!"
+  user_input = "'; DROP TABLE users; --"
   print(vulnerable_function(user_input))
- 
